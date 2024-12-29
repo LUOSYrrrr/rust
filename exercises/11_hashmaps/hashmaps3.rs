6c25f1691ecc80cb2864dd11249d5fc91eb4aeb4
LUOSYrrrr <1257480002@qq.com>
2024-12-29 16:27:37 +1100

    11-fix

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -34,6 +34,10 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_1 = scores.entry(team_1_name).or_insert(TeamScores::default());
         team_1.goals_scored += team_1_score;
         team_1.goals_conceded += team_2_score;
+        let team_2 = scores.entry(team_2_name).or_insert(TeamScores::default());
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
+        
 
     }
 
